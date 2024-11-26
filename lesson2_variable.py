c645f59d175eaf250dc6a6406d5c6aae963e544d
andrey1968-dotcom <cushvintsev36@gmail.com>
2024-11-26 20:52:19 +0500

    Add files via upload

---
+++ lesson2_variable.py
@@ -0,0 +1,8 @@
+number_of_completed_homework_assignments = "12,"
+number_of_hours_spent = "1.5,"
+course_name = '"Python"'
+print(1.5/12)
+time_for_one_task = "0.125"
+print('Kurs: '+ course_name + ', vsego zadach: '+ number_of_completed_homework_assignments + ' zatracheno chasov: ' + number_of_hours_spent + ' srednee vremya vyipolneniya: '+ time_for_one_task + ' chasa.')
+
+
